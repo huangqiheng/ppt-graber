0624be6917cd38eb91844b9079e3b997cce7c0c1
and <and@hqh.me>
2017-04-05 18:39:59 +0800

    update

--- ffmpeg-input.sh
+++ ffmpeg-input.sh
@@ -5,7 +5,7 @@ is_run=$(ps -ef | grep ffmpeg-input.sh | grep -v grep | wc -l)
 if [ "$is_run" = "1" ]; then
 	echo 'is running'
 else
-	/usr/bin/ffmpeg  -v quiet -f v4l2 -r 30 -s 1920x1080 -i /dev/video0  -f v4l2 -r 30 -s 1280x720 /dev/video1 -f v4l2 -r 30 /dev/video2
+/usr/bin/ffmpeg  -v quiet -f v4l2 -s 1280x720 -i /dev/video0 -codec copy -f v4l2 /dev/video1 -codec copy -f v4l2 /dev/video2
 fi
 
 exit 0
